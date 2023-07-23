138941506933e584b79536217252908de859b7cd
dev-taeyeong <dev.taeyeong@gmail.com>
2023-07-24 01:14:18 +0900

    add - Money에 대한 통화 변환을 수행하는 Reduce

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -58,5 +59,18 @@ class AppTest {
         Bank bank = new Bank();
         Money result = bank.reduce(Money.dollar(1), "USD");
         assertThat(result).isEqualTo(Money.dollar(1));
+    }
+
+    @Test
+    public void testReduceMoneyDifferentCurrency() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertThat(result).isEqualTo(Money.dollar(1));
+    }
+
+    @Test
+    public void testIdentityRate() {
+        assertThat(new Bank().rate("USD", "USD")).isEqualTo(1);
     }
 }
