c0dd594fe1fba9e3749f4e847b8489a97f3e0fa9
dev-taeyeong <dev.taeyeong@gmail.com>
2023-07-23 23:19:37 +0900

    refactoring - amount를 private으로 만들기

---
+++ src/test/java/org/example/DollarTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        assertThat(five.times(2)).isEqualTo(new Dollar(10));
+        assertThat(five.times(3)).isEqualTo(new Dollar(15));
+    }
+
+    @Test
+    public void testEquals() {
+        assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
+        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
+    }
+}
