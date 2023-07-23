fa3e87998748839c518c0328a3058d3a74dffb20
dev-taeyeong <dev.taeyeong@gmail.com>
2023-07-24 01:52:06 +0900

    clean import

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,9 +1,7 @@
 package org.example;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AppTest {
