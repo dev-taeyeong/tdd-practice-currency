23def342ae98b3c18b631124f638b9a24eb2ad46
dev-taeyeong <dev.taeyeong@gmail.com>
2023-07-24 01:47:09 +0900

    end

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -84,4 +84,26 @@ class AppTest {
                 fiveBucks.plus(tenFrancs), "USD");
         assertThat(result).isEqualTo(Money.dollar(10));
     }
+
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrances = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrances).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertThat(result).isEqualTo(Money.dollar(15));
+    }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertThat(result).isEqualTo(Money.dollar(20));
+    }
 }
